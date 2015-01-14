4e31a5344260f119d91b64a8930017bc010fdf91
Thomas Leonard <talex5@gmail.com>
2015-01-14 11:01:07 +0000

    Switched to https for better security

--- config.ml
+++ config.ml
@@ -1,9 +1,9 @@
 open Mirage
 
 let queue = foreign
-  ~libraries:["cstruct.syntax"]
-  ~packages:["cstruct"]
-  "Unikernel.Main" (console @-> block @-> http @-> job)
+  ~libraries:["cstruct.syntax";"mirage-http";"x509";"mbr-format.mirage"]
+  ~packages:["cstruct";"mirage-http";"x509";"mbr-format"]
+  "Unikernel.Main" (console @-> block @-> conduit @-> kv_ro @-> job)
 
 let net =
   match get_mode () with
@@ -20,13 +20,15 @@ let stack console =
   | `Direct -> direct_stackv4_with_default_ipv4 console tap0
   | `Socket -> socket_stackv4 console [Ipaddr.V4.any]
 
-let server =
-  http_server (`TCP (`Port 8080)) (conduit_direct (stack default_console))
+let conduit =
+  conduit_direct ~tls:(tls_over_conduit default_entropy) (stack default_console)
 
 let storage =
-  block_of_file "xvda"
+  match get_mode () with
+  | `Xen -> block_of_file "xvda"
+  | `Unix -> block_of_file "disk.img"
 
 let () =
   register "queue" [
-    queue $ default_console $ storage $ server
+    queue $ default_console $ storage $ conduit $ crunch "keys"
   ]
--- unikernel.ml
+++ unikernel.ml
@@ -3,15 +3,49 @@
 
 open Lwt
 
-module Main (C : V1_LWT.CONSOLE) (B : V1_LWT.BLOCK) (H : Cohttp_lwt.Server) :
-  sig
-    val start : C.t -> B.t -> (H.t -> unit Lwt.t) -> unit Lwt.t
-  end = struct
-    module Q = Upload_queue.Make(B)
+let (>>|=) x f =
+  x >>= function
+  | `Error (`Unknown msg) -> failwith msg
+  | `Error _ -> failwith "Error"
+  | `Ok v -> f v
+
+let (--) = Int64.sub
+
+let private_key_len = 887
+let key_partition_size = 4096
+
+let round_up bytes block_size =
+  let blocks = (bytes + block_size - 1) / block_size in
+  blocks * block_size
+
+module Main (C : V1_LWT.CONSOLE) (B : V1_LWT.BLOCK) (Conduit : Conduit_mirage.S) (CertStore : V1_LWT.KV_RO) =
+  struct
+    module Part = Mbr_partition.Make(B)
+    module Q = Upload_queue.Make(Part)
+    module Http = HTTP.Make(Conduit)
+    module H = Http.Server
+
+    let read_from_partition b ~len =
+      Part.get_info b >>= fun info ->
+      let buf_size = round_up len info.Part.sector_size in
+      let pages_needed = Io_page.round_to_page_size buf_size / Io_page.round_to_page_size 1 in
+      let page_buf = Io_page.get pages_needed |> Io_page.to_cstruct in
+      let buf = Cstruct.sub page_buf 0 buf_size in
+      Part.read b 0L [buf] >>|= fun () ->
+      return (Cstruct.sub buf 0 len)
+
+    let read_from_kv kv name =
+      let (>>|=) x f =
+        x >>= function
+        | `Ok x -> f x
+        | `Error (CertStore.Unknown_key _) -> failwith name in
+      CertStore.size kv name >>|= fun len ->
+      CertStore.read kv name 0 (Int64.to_int len) >>|= fun bufs ->
+      return (Cstruct.copyv bufs |> Cstruct.of_string)
 
     let unsupported_method = H.respond_error ~status:`Bad_request ~body:"Method not supported\n" ()
 
-    let put q request body =
+    let put q request body ~user =
       match Cohttp.Header.get request.Cohttp.Request.headers "Content-Length" with
       | None -> H.respond_error ~status:`Length_required ~body:"Missing Content-Length\n" ()
       | Some len ->
@@ -19,6 +53,7 @@ module Main (C : V1_LWT.CONSOLE) (B : V1_LWT.BLOCK) (H : Cohttp_lwt.Server) :
             size = Int64.of_string len;
             data = Cohttp_lwt_body.to_stream body;
           } in
+          Log.info "%Ld byte item uploaded by %s" item.Upload_queue.size user >>= fun () ->
           Q.Upload.add q item >>= function
           | `Ok () -> H.respond_string ~status:`OK ~body:"Accepted\n" ()
           | `Error (`Unknown ex) -> raise ex
@@ -43,9 +78,9 @@ module Main (C : V1_LWT.CONSOLE) (B : V1_LWT.BLOCK) (H : Cohttp_lwt.Server) :
       Q.Download.delete q >>= fun () ->
       H.respond_string ~status:`OK ~body:"OK\n" ()
 
-    let handle_uploader q request body =
+    let handle_uploader q request body ~user =
       match H.Request.meth request with
-      | `PUT -> put q request body
+      | `PUT -> put q request body ~user
       | `POST -> H.respond_error ~status:`Bad_request ~body:"Use PUT, not POST\n" ()
       | _ -> unsupported_method
 
@@ -55,26 +90,53 @@ module Main (C : V1_LWT.CONSOLE) (B : V1_LWT.BLOCK) (H : Cohttp_lwt.Server) :
       | `DELETE -> delete q
       | _ -> unsupported_method
 
-    let start c b http =
+    let bad_path path =
+      H.respond_error ~status:`Bad_request ~body:(Printf.sprintf "Bad path '%s'\n" path) ()
+
+    let re_endpoint = Str.regexp "^/\\(uploader\\|downloader\\)/!\\(.*\\)$"
+
+    let callback q _conn_id request body =
+      try_lwt
+        let path = Uri.path request.Cohttp.Request.uri in
+        if Str.string_match re_endpoint path 0 then (
+          let label = Str.matched_group 1 path in
+          let swiss_hash = Str.matched_group 2 path
+            |> Cstruct.of_string
+            |> Nocrypto.Hash.digest `SHA256
+            |> Cstruct.to_string
+            |> B64.encode in
+          match label, swiss_hash with
+          | "uploader", "kW8VOKYP1eu/cWInpJx/jzYDSJzo1RUR14GoxV/CImM=" -> handle_uploader q request body ~user:"Alice"
+          | "downloader", "PEj3nuboy3BktVGzi9y/UBmgkrGuhHD1i6WsXDw1naI=" -> handle_downloader q request
+          | _ -> bad_path path
+        ) else bad_path path
+      with ex ->
+        Log.warn "error handling HTTP request: %s\n%s"
+          (Printexc.to_string ex)
+          (Printexc.get_backtrace ()) >>= fun () ->
+        raise ex
+
+    let conn_closed _conn_id =
+      Log.info "connection closed" |> ignore
+
+    let start c b conduit cert_store =
       Log.write := C.log_s c;
       Log.info "start in queue service" >>= fun () ->
 
-      Q.create b >>= fun q ->
+      B.get_info b >>= fun info ->
+      let key_sectors = (key_partition_size + info.B.sector_size - 1) / info.B.sector_size |> Int64.of_int in
+      let queue_sectors = info.B.size_sectors -- key_sectors in
 
-      let callback _conn_id request body =
-        try_lwt
-          match Uri.path request.Cohttp.Request.uri with
-          | "/uploader" -> handle_uploader q request body
-          | "/downloader" -> handle_downloader q request
-          | path -> H.respond_error ~status:`Bad_request ~body:(Printf.sprintf "Bad path '%s'\n" path) ()
-        with ex ->
-          Log.warn "error handling HTTP request: %s\n%s"
-            (Printexc.to_string ex)
-            (Printexc.get_backtrace ()) >>= fun () ->
-          raise ex in
+      Part.(connect {b; start_sector = 0L; length_sectors = key_sectors}) >>|= fun key_partition ->
+      Part.(connect {b; start_sector = key_sectors; length_sectors = queue_sectors}) >>|= fun queue_partition ->
 
-      let conn_closed _conn_id =
-        Log.info "connection closed" |> ignore in
+      Q.create queue_partition >>= fun q ->
 
-      http (H.make ~callback ~conn_closed ())
+      lwt certs = read_from_kv cert_store "tls/server.pem" >|= X509.Cert.of_pem_cstruct
+      and pk = read_from_partition key_partition ~len:private_key_len >|= X509.PK.of_pem_cstruct1 in
+      let certificates = `Single (certs, pk) in
+      let tls_config = Tls.Config.server ~certificates () in
+      let spec = H.make ~callback:(callback q) ~conn_closed () in
+      let mode = `TLS (tls_config, `TCP (`Port 8443)) in
+      Conduit.serve ~ctx:conduit ~mode (H.listen spec)
   end
